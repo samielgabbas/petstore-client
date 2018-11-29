4642e825245e15da8e39fe58679a8e69168dd7ab
samielgabbas <sami.elgabbas>
2018-11-29 17:35:06 +0100

    update server.js config file

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/petstore-client'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/petstore-client/index.html'));
+res.sendFile(`./dist/petstore-client/index.html`);
 });
 
 // Start the app by listening on the default Heroku port
